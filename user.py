cf8909d23fac63d7d770f04d86f9d6bc1ac4ba67
Grizz03 <steve.grizz03@gmail.com>
2021-05-22 16:29:27 -0700

    multiple filter methods to iterate over movies list

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+from movie import Movie
+
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -7,8 +10,13 @@ class User:
     def __repr__(self):
         return '<User {}'.format(self.name)
 
+    def add_movie(self, name, genre):
+        self.movies.append(Movie(name, genre, False))
+
+    def delete_movie(self, name):
+        list(filter(lambda movie: movie.name != name, self.movies))
+
     #  Filter method
     def watched_movies(self):
         # If false will remove from list
         return list(filter(lambda x: x.watched, self.movies))
-
